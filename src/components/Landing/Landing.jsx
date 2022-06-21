2d6bc39a4cda554f1cf9c414dcb2c95dc0a30cd0
lucascencig <cenciglucas@gmail.com>
2022-06-21 13:56:55 -0300

    landing terminada y galeria de imagenes lista

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
 import S from './Landing.module.css';
+import logoHenry from '../../utils/fotos/henryclubLOGO.gif';
 import { Link } from 'react-router-dom';
 
 export default function Landing() {
   return (
     <div className={S.contenedorGeneral}>
       <div className={S.titulo}>
-        <h1>Titulo</h1>
+        <h1>Henry Club</h1>
       </div>
       <div className={S.logo}>
         {/* aca va el logo */}
-        <img src="" alt="" />
+        <img src={logoHenry} alt="" />
       </div>
 
-      <div className={S.btnRegistro}>
-        <Link to={'/register'}>
-          <button>Registrate</button>
-        </Link>
-      </div>
+      <div className={S.botones}>
+        <div className={S.btnIngresar}>
+          <Link to={'/home'}>
+            <button>
+              <span>Ingresar</span>
+            </button>
+          </Link>
+        </div>
 
-      <Link to={'/home'}>
-        <button>Ingresar</button>
-      </Link>
+        <div className={S.btnRegistro}>
+          <Link to={'/register'}>
+            <button>
+              <span>Registrate</span>
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
