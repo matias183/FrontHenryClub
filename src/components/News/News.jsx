22cf694e25014540756d9b1d12eee171787125ff
lucascencig <cenciglucas@gmail.com>
2022-06-22 17:56:33 -0300

    correccion news

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './News.css';
 
-export default function Noticias({title, subtitle, image, id}) {
+export default function Noticias({ title, subtitle, image, id }) {
   return (
     <div>
       <div>
@@ -15,64 +15,13 @@ export default function Noticias({title, subtitle, image, id}) {
       <div>
         <h1 className="titleNews">Noticias del club</h1>
         <div className="newsContainer">
-          <div className='news'>
-            <img src={image} alt='img not found' className='imgNews'/>
+          <div className="news">
+            <img src={image} alt="img not found" className="imgNews" />
             <h2>{title}</h2>
             <span>{subtitle}</span>
-            <Link to={'/news/' + id}>
-              Leer Más...
-            </Link>
+            <Link to={'/news/' + id}>Leer Más...</Link>
           </div>
         </div>
-
-      </div>
-
-      <div className="seccionComentarios">
-        <section>
-          <h3>Comentarios:</h3>
-          <div className="inputName">
-            <label htmlFor="">Nombre</label>
-            <input onChange={validarInputName} type="text" />
-            <div className="error">
-              <p>
-                {error !== 'Los datos no son validos.' ? null : (
-                  <p>El nombre no puede estar vacio.</p>
-                )}
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <textarea
-              name=""
-              id=""
-              cols="50"
-              rows="5"
-              placeholder="Escribe tu comentario..."
-            ></textarea>
-          </div>
-          <div className="enviarComentario">
-            <button onSubmit={handleSubmit} type="submit">
-              <span>Enviar</span>
-            </button>
-          </div>
-        </section>
-
-        {/* NECESITO TENER ESTADO DE REDUX PARA MAPEAR COMENTARIOS Y QUE SE VEAN CUANDO SE SUBMITEEN, HACER UN LOCAL STATE*/}
-        <section>
-          <div className="seccionComentariosHechos">
-            <h3>Comentarios:</h3>
-            <div>
-              <div className="comentariosHechos">
-                {/* <p>{localState."algo".join('<br/> ')}</p> */}
-                <p></p>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-      <div className="footer">
-        <Footer />
       </div>
     </div>
   );
