10cd3b9f0816ed4a4d565e484e2136a41ab570d1
lucascencig <cenciglucas@gmail.com>
2022-06-23 15:08:04 -0300

    form crear news

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Landing from './components/Landing/Landing.jsx'
-import Home from './components/Home/Home.jsx'
-import Reserve from './components/Form Reserve/Reserve.jsx'
-import Login from './components/FormLogin/Login.jsx'
-import GaleriaImg from './components/Galeria de imagenes/GaleriaImg.jsx'
-import Profile from './Profile/Profile.jsx'
+import Landing from './components/Landing/Landing.jsx';
+import Home from './components/Home/Home.jsx';
+import Reserve from './components/Form Reserve/Reserve.jsx';
+import Login from './components/FormLogin/Login.jsx';
+import GaleriaImg from './components/Galeria de imagenes/GaleriaImg.jsx';
+import Profile from './Profile/Profile.jsx';
 import Register from './components/Form Register/Register';
 import News from './components/News/News.jsx';
-import { IconName } from "react-icons/fa";
+import CrearNoticia from './components/Form CrearNoticia/CrearNoticia';
+import { IconName } from 'react-icons/fa';
 import Contact from './components/Contact/contact.jsx';
 import Activity from './components/Form Create Activities/NewActivity.jsx';
-
-
 
 function App() {
   return (
     <div className="App">
       <Switch>
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/reserve" component={Reserve} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/galery" component={GaleriaImg} />
-      <Route exact path="/profile/:id" component={Profile} />
-      <Route exact path="/news" component={News} />
-      <Route exact path="/contact-us" component={Contact} />
-      <Route exact path="/newactivity" component={Activity} />
-      <Route exact path="*" component={Landing} />
-      
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/reserve" component={Reserve} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/galery" component={GaleriaImg} />
+        <Route exact path="/profile/:id" component={Profile} />
+        <Route exact path="/news" component={News} />
+        <Route exact path="/contact-us" component={Contact} />
+        <Route exact path="/newactivity" component={Activity} />
+        <Route exact path="/createNews" component={CrearNoticia} />
+        <Route exact path="*" component={Landing} />
       </Switch>
     </div>
   );
