4190d7ba4d6d6c32024839675ca2994f980d260a
ZurdoAsd <gaspar.ajs@gmail.com>
2022-06-24 15:20:43 -0300

    mod panel user y admin

--- src/User/User.jsx
+++ src/User/User.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
+import { Route, NavLink} from 'react-router-dom';
+import Home from '../PAdmin/Home2/Home2';
+import News from '../PAdmin/News2/News2';
+import Header from "../navbar/navbar"
+import s from './User.module.css'
+
 
 export default function User() {
   return (
-    <div>User</div>
+    <> 
+  <div className={s.header}>
+<Header/>
+  </div>
+  <div className={s.container}>
+   <div className={s.barralateral}>
+     <ul>
+      <li>
+      <NavLink to="/user/news">News</NavLink>
+ 
+      </li>
+      <li> 
+      <NavLink to="/user/home">Home</NavLink>
+      </li>
+      <li> 
+      <NavLink to="/user/home">Your Calendar</NavLink>
+      </li>
+      <li> 
+      <NavLink to="/user/home">modify your data</NavLink>
+      </li>
+
+     </ul>
+    </div>
+    <div className={s.mostrar}>
+     <Route path="/user/home" component={Home} />
+     <Route path="/user/news" component={News} />
+     
+    
+    </div>
+    </div>
+    </>
   )
 }
