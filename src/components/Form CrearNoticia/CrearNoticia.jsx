3bd70ae3031993b1c8d0661fa39c5f9cf1042f93
lucascencig <cenciglucas@gmail.com>
2022-06-23 21:27:00 -0300

    create news arreglado

--- src/components/Form CrearNoticia/CrearNoticia.jsx
+++ src/components/Form CrearNoticia/CrearNoticia.jsx
@@ -1,39 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-export default function CrearAnuncio() {
-  return (
-    <div>
-      <Link to={'/home'}>
-        <button>
-          <span>Volver</span>
-        </button>
-      </Link>
-      <h1>Crear Noticias</h1>
-      <form>
-        <label htmlFor="">Titulo de Noticia: </label>
-        <input type="text" />
-
-        <label htmlFor="">Subtitulo de Noticia: </label>
-        <input type="text" />
-
-        <label htmlFor="">Descripción de Noticia: </label>
-        <textarea
-          name=""
-          id=""
-          cols="40"
-          rows="5"
-          placeholder="Escribe la noticia"
-        ></textarea>
-
-        <label htmlFor="">Imagen de la Noticia: </label>
-        <input type="file" />
-
-        <button type="submit">Publicar</button>
-      </form>
-    </div>
-  );
-}
 import React, { useEffect, useState } from 'react';
 import { CreateNews } from '../../redux/Actions/Action';
 import { Link } from 'react-router-dom';
