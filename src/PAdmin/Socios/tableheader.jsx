8069cf04355cd0420f0a36d405306b1e49feb70d
ZurdoAsd <gaspar.ajs@gmail.com>
2022-07-02 16:52:27 -0300

    mergeame esta

---
+++ src/PAdmin/Socios/tableheader.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import { TableSortLabel,TableHead,TableCell,TableRow} from "@material-ui/core";
+
+export default function TableHeader(props) {
+
+  const{valueToOrderBy,orderDirection,handleRequestSort} = props;
+
+ const createSortHandler = (property) =>(event) =>{
+  handleRequestSort(event, property);
+ };
+
+console.log(valueToOrderBy)
+  return (
+
+ <TableHead>
+
+<TableRow>
+
+  <TableCell key="membershipNumber">
+  <TableSortLabel 
+active={valueToOrderBy==="membershipNumber"} 
+direction={valueToOrderBy==="membershipNumber"?orderDirection:"asc"}
+onClick={createSortHandler("membershipNumber")}
+> N° Socio</TableSortLabel></TableCell>
+
+<TableCell key="name">
+  <TableSortLabel
+  active={valueToOrderBy==="name"} 
+  direction={valueToOrderBy==="name"?orderDirection:"asc"}
+  onClick={createSortHandler("name")}
+  > 
+   Name 
+ </TableSortLabel>
+</TableCell>
+
+
+<TableCell key="surname">
+  <TableSortLabel
+  active={valueToOrderBy==="surname"} 
+  direction={valueToOrderBy==="surname"?orderDirection:"asc"}
+  onClick={createSortHandler("surname")}
+  > 
+  Surname 
+ </TableSortLabel>
+</TableCell>
+
+  <TableCell key="username">
+  <TableSortLabel
+  active={valueToOrderBy==="username"} 
+  direction={valueToOrderBy==="username"?orderDirection:"asc"}
+  onClick={createSortHandler("username")}
+  > 
+username
+ </TableSortLabel>
+</TableCell>
+
+
+  <TableCell key="address">
+  <TableSortLabel
+  active={valueToOrderBy==="address"} 
+  direction={valueToOrderBy==="address"?orderDirection:"asc"}
+  onClick={createSortHandler("address")}> 
+ address
+ </TableSortLabel>
+</TableCell>
+
+  <TableCell key="phone">
+  <TableSortLabel
+  active={valueToOrderBy==="phone"} 
+  direction={valueToOrderBy==="phone"?orderDirection:"asc"}
+  onClick={createSortHandler("phone")}> 
+phone
+ </TableSortLabel>
+</TableCell>
+
+<TableCell key="email">
+  <TableSortLabel
+  active={valueToOrderBy==="email"} 
+  direction={valueToOrderBy==="email"?orderDirection:"asc"}
+  onClick={createSortHandler("email")}> 
+email
+ </TableSortLabel>
+</TableCell>
+
+<TableCell key="role">
+  <TableSortLabel
+  active={valueToOrderBy==="role"} 
+  direction={valueToOrderBy==="role"?orderDirection:"asc"}
+  onClick={createSortHandler("role")}> 
+Rol
+ </TableSortLabel>
+</TableCell>
+
+
+<TableCell key="dni">
+  <TableSortLabel
+  active={valueToOrderBy==="dni"} 
+  direction={valueToOrderBy==="dni"?orderDirection:"asc"}
+  onClick={createSortHandler("dni")}> 
+Dni
+ </TableSortLabel>
+</TableCell>
+
+
+<TableCell>
+<TableSortLabel> 
+  Acciones
+  </TableSortLabel>
+  </TableCell>
+
+
+
+
+</TableRow>
+
+</TableHead>
+
+  )
+}
+
--- src/components/Form Create Activities/NewActivity.jsx
+++ src/components/Form Create Activities/NewActivity.jsx
@@ -12,41 +12,16 @@ export default function NewActivity() {
   const state = useSelector(state => state)
 
   useEffect(() => {
-    dispatch(getCategory())
-    dispatch(getTeacher())
-    dispatch(getSport())
-  }, [])
+    dispatch(getCategory());
+    dispatch(getTeacher());
+    dispatch(getSport());
+  }, [dispatch])
 
   const [loading, setLoading] = useState(false);
   const [input, setInput] = useState(
     {
 
     });
-
-
-  // const categorias = [{name:"infantil", categoryId: 1},
-  //   {name:"recreativo", categoryId: 2},
-  //   {name:"senior", categoryId: 3}
-
-  // ];
-
-  // const Sport =
-
-  // [{name:"Tenis",  sportId: 1},
-  //   {name:"Futbol",  sportId: 2},
-  //   {name:"Hockey",  sportId: 3},
-  //   {name:"Natacion",  sportId: 4},
-  //   {name:"Pileta-Libre",  sportId: 5},
-
-  // ];
-
-  // console.log(Sport);
-
-  // const  Instructor = [
-  //   {name: "Carlos", teacherId: 1},
-  //   {name: "Juan", teacherId: 2},
-  //   {name: "Lara", teacherId: 3},
-  // ]; dispatch(deleteComment(id))
 
   const [errors, setErrors] = useState({
   })
@@ -73,84 +48,28 @@ export default function NewActivity() {
     }));
   };
   console.log(input)
-
-
-
-  const handleSelectcategoryId = (e) => {
-    e.preventDefault();
-    setInput({
-      ...input,
-      categoryId: [...new Set([...input.categoryId, e.target.value])],
-    });
-
-  };
-  const handleSelectInstructor = (e) => {
-    e.preventDefault();
-    setInput({
-      ...input,
-      teacherId: [...new Set([...input.teacherId, e.target.value])],
-    });
-
-  };
-  const handleSelectSport = (e) => {
-    e.preventDefault();
-    setInput({
-      ...input,
-      sportId: [...new Set([...input.sportId, e.target.value])],
-    });
-  };
-
-
-  const handleTeacher = (e) => {
-    e.preventDefault();
-    let filtro = input.teacherId.filter((f) => f === e.target.value);
-    setInput({
-      ...input,
-      teacherId: [...filtro],
-    });
-
-  };
-
-
-  const handleSport = (e) => {
-    e.preventDefault();
-    let filtro = input.sportId.filter((f) => f === e.target.value);
-    setInput({
-      ...input,
-      sportId: [...filtro],
-    });
-
-  };
-
-  const handleCategory = (e) => {
-    e.preventDefault();
-    let filtro = input.categoryId.filter((f) => f === e.target.value);
-    setInput({
-      ...input,
-      categoryId: [...filtro],
-    });
-
-  };
+console.log(state)
 
 
 
   const onSubmit = (e) => {
     e.preventDefault();
     if (input.start !== "" && input.days !== "" && input.finish !== "" &&
-      input.fee !== "" && input.teacherId !== "" && input.sportId !== "" && input.description !== "" && input.categoryId !== "");
+      input.fee !== "" && input.teacherId !== "" && input.sportId !== "" && 
+      input.description !== "" && input.categoryId !== "");
     else if (
       !Object.keys(errors).length) {
       setLoading(true);
       dispatch(loginMember())
       setInput({
-        // days: "",
-        // start: "",
-        // finish: "",
-        // fee: "",
-        // teacherId: "",
-        // sportId: "",
-        // categoryId: "",
-        // description: "",
+        days: "",
+        start: "",
+        finish: "",
+        fee: "",
+        teacherId: "",
+        sportId: "",
+        categoryId: "",
+        description: "",
       });
       setLoading(false);
     }
@@ -206,19 +125,16 @@ export default function NewActivity() {
           {errors.fee && <p className={s.errors}>{errors.fee}</p>}
 
           <div>
-            <select className={s.input} onChange={handleSelect} name="teacher">
+            <select className={s.select} onChange={handleSelect} name="teacher">
               <option value=""> Select Instructor</option>
-              {state.teacher &&
-                state.teacher.map((teacher) => (
-                  <option key={teacher.id} value={teacher.id}>
-                    {teacher.name}
-                  </option>
-                ))}
+             {/* {state.teacher&&state.teacher.map((teacher) => (
+                  <option key={teacher.id} value={teacher.id}>{teacher.name}</option>
+                ))}  */}
             </select>
           </div>
 
           <div>
-            <select className={s.input} onChange={handleSelect} name="sport">
+            <select className={s.select} onChange={handleSelect} name="sport">
               <option value=""> Select Deporte</option>
               {state.sport &&
                 state.sport.map((sport) => (
@@ -230,7 +146,7 @@ export default function NewActivity() {
           </div>
 
           <div>
-            <select className={s.input} onChange={handleSelect} name="category">
+            <select className={s.select} onChange={handleSelect} name="category">
               <option value=""> Select Category</option>
               {state.category &&
                 state.category.map((category) => (
@@ -240,113 +156,6 @@ export default function NewActivity() {
                 ))}
             </select>
           </div>
-
-          {/* <div>     
-                <div >
-              <label >Instructor</label>
-              <select className={s.input} onChange={handleSelectInstructor}>
-                <option value="" hidden> Select Instructor</option>
-               
-                {  input.teacherId.length<1?
-             
-               Instructor?.map((e) => { 
-                return( <option key={e.teacherId} value={e.teacherId}>{e.name}</option>)
-                })
-               : <option value="" hidden> Select Instructor</option>
-                }
-                  </select>
-                 </div>
-                   {errors.teacherId && <p className={s.errors}>{errors.teacherId}</p>}  
-                   </div>
-                   <div>
-                    
-            {input.teacherId &&input.teacherId.map((e) => (
-              <div  key={e.teacherId}>
-                <button
-                  key={e.teacherId}
-                  type="button"
-                  value={e.teacherId}
-                  onClick={handleTeacher}
-                >
-                  {e.toUpperCase()}
-                </button>
-              </div>
-            ))
-            }
-          </div>
-
-
-
-                  <div>     
-                <div >
-              <label >Sport</label>
-              <select className={s.input} onChange={handleSelectSport}>
-                <option value="" hidden> Select Sport</option>
-                { input.sportId.length<1?
-             Sport?.map((e) => { 
-              return (   <option key={e.sportId} value={e.sportId}>{e.name}</option>) }
-             )
-             : <option value="" hidden>Select Sport</option>
-                }
-                  </select>
-                 </div>
-                 {errors.sportId && <p className={s.errors}>{errors.sportId}</p>}
-                   </div>
-
-                   <div>
-                    
-                    {input.sportId &&input.sportId.map((e) => (
-                      <div  key={e.sportId}>
-                        <button
-                          key={e.sportId}
-                          type="button"
-                          value={e.sportId}
-                          onClick={handleSport}
-                        >
-                          {e.toUpperCase()}
-                        </button>
-                      </div>
-                    ))
-                    }
-                  </div>
-
-
-
-
-                   <div>     
-                <div >
-              <label >categoria</label>
-              <select className={s.input} onChange={handleSelectcategoryId}>
-                <option value="" hidden> Select categoria</option>
-                {  input.categoryId.length<1?
-               categorias?.map((e) => (
-                <option key={e.categoryId} value={e.categoryId}>{e.name}</option>))
-                : <option value="" hidden>Select categoria</option>
-                }
-                  </select>
-                 </div>
-                 {errors.categoryId && <p className={s.errors}>{errors.categoryId}</p>}
-                   </div>
-
-                   <div>
-                    
-                    {input.categoryId &&input.categoryId.map((e) => (
-                      <div  key={e.categoryId}>
-                        <button
-                          key={e.categoryId}
-                          type="button"
-                          value={e.categoryId}
-                          onClick={handleCategory}
-                        >
-                          {e.toUpperCase()}
-                        </button>
-                      </div>
-                    ))
-                    }
-                  </div> */}
-
-
-
           <textarea className={s.input}
             onChange={HandleChange}
             value={input.description}
